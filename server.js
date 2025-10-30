513d87eb6306bde337177c7132ce6f04d8197029
goulartduarda <arda402010@hotmail.com>
2025-10-30 19:28:35 -0300

    fix: correção

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 // ============================================================
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg"); // ✅ driver certo pro Supabase (PostgreSQL)
 
 const app = express();
 
